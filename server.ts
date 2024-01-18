dc8aa316211f501f07e85fbef03d1ec6356e6ee6
Tonipenyallop <kimtesu521@gmail.com>
2024-01-18 22:42:43 +0900

    update:server

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
-Deno.serve(async (_request: Request) => {
+import { SERVER_PORT } from "./ports.ts";
+Deno.serve({ port: SERVER_PORT }, (_request: Request) => {
+  console.log("server received request mate");
   // 1. after waiting 1 second
   return new Promise((resolve, reject) => {
     try {
@@ -10,5 +12,3 @@ Deno.serve(async (_request: Request) => {
     }
   });
 });
-
-// 3. load balancer
