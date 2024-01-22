5a2f733ab46e5af56adf8379434a93f75434c8a3
Tonipenyallop <kimtesu521@gmail.com>
2024-01-22 21:49:22 +0900

    update: load balancer

--- load_balancer.ts
+++ load_balancer.ts
@@ -1,10 +1,27 @@
-import { SERVER_PORT, LOAD_BALANCER_PORT } from "./ports.ts";
+import {
+  SERVER_PORT,
+  SERVER2_PORT,
+  SERVER3_PORT,
+  LOAD_BALANCER_PORT,
+} from "./ports.ts";
+
+let currentIndex = 0;
+
+const servers = [SERVER_PORT, SERVER2_PORT, SERVER3_PORT];
+const getServer = () => {
+  const server = servers[currentIndex];
+
+  // moving current index to one step forward
+  if (currentIndex === 2) currentIndex = 0;
+  else currentIndex += 1;
+
+  return server;
+};
 
 Deno.serve({ port: LOAD_BALANCER_PORT, hostname: "0.0.0.0" }, async (_req) => {
-  console.log("request received");
+  const serverPort = getServer();
+  const response = await fetch(`http://localhost:${serverPort}/`);
 
-  const response = await fetch(`http://localhost:${SERVER_PORT}/`);
-  console.log("response");
-  console.log(response);
+  // const response = await fetch(`http://localhost:${SERVER_PORT}/`);
   return response;
 });
