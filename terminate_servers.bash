bb9cd4a3508d1583fccc00bd29bcd49e90d95835
Tonipenyallop <kimtesu521@gmail.com>
2024-01-22 21:48:07 +0900

    update: bash

--- terminate_servers.bash
+++ terminate_servers.bash
@@ -1,1 +1,1 @@
-lsof -i tcp:8000 -t | xargs kill && lsof -i tcp:8001 -t | xargs kill && lsof -i tcp:8888 -t | xargs kill
+lsof -i tcp:8000 -t | xargs kill && lsof -i tcp:8001 -t | xargs kill && lsof -i tcp:8002 -t | xargs kill && lsof -i tcp:8888 -t | xargs kill
