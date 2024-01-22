b457575bd3a24df6d1ffd85451f783dfd00ef423
Tonipenyallop <kimtesu521@gmail.com>
2024-01-22 21:48:37 +0900

    update: server

--- server.ts
+++ server.ts
@@ -1,12 +1,34 @@
-import { SERVER_PORT } from "./ports.ts";
+import { SERVER_PORT, SERVER2_PORT, SERVER3_PORT } from "./ports.ts";
 Deno.serve({ port: SERVER_PORT }, (_request: Request) => {
-  console.log("server received request mate");
-  // 1. after waiting 1 second
   return new Promise((resolve, reject) => {
     try {
-      setTimeout(() => {
-        resolve(new Response("TONI BLOG!"));
-      }, 1000);
+      // setTimeout(() => {
+      resolve(new Response("TONI BLOG!"));
+      // }, 1000);
+    } catch (err) {
+      reject(new Response(`err while sending response: ${err}`));
+    }
+  });
+});
+
+Deno.serve({ port: SERVER2_PORT }, () => {
+  return new Promise((resolve, reject) => {
+    try {
+      // setTimeout(() => {
+      resolve(new Response("TONI BLOG2!"));
+      // }, 1000);
+    } catch (err) {
+      reject(new Response(`err while sending response: ${err}`));
+    }
+  });
+});
+
+Deno.serve({ port: SERVER3_PORT }, () => {
+  return new Promise((resolve, reject) => {
+    try {
+      // setTimeout(() => {
+      resolve(new Response("TONI BLOG3!"));
+      // }, 1000);
     } catch (err) {
       reject(new Response(`err while sending response: ${err}`));
     }
--- server2.ts
+++
@@ -1,4 +0,0 @@
-import { SERVER2_PORT } from "./ports.ts";
-Deno.serve({ port: SERVER2_PORT }, () => {
-  return new Response("server2");
-});
