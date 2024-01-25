c35d2fe4f9db4221e19793144a9ba80fb3b428ac
Tonipenyallop <kimtesu521@gmail.com>
2024-01-25 22:28:49 +0900

    upadte: bash command

--- init_servers.bash
+++ init_servers.bash
@@ -1,1 +1,10 @@
-deno run --allow-net server.ts & deno run --allow-net load_balancer.ts
+if [ $# -eq 0 ]
+then
+    echo "PORT should be provided"
+    exist 1
+fi
+
+PORT=$1
+
+deno run --allow-net server.ts $PORT
+
